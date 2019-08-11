76dfe5a82b89d6397bdf9c78f0212a0c4e44987f
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-08-11 18:08:16 -0300

    create: adding migration and model of the meetup

---
+++ src/app/models/Meetup.js
@@ -0,0 +1,27 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Meetup extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        description: Sequelize.TEXT,
+        location: Sequelize.STRING,
+        times: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.File, { foreignKey: 'image_id', as: 'banner' });
+  }
+}
+
+export default Meetup;
