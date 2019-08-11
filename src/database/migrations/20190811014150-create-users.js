4511b89d7732686a25f1b55e9e1f5bf96c3e0199
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-08-10 22:58:02 -0300

    create: configuring sequelize and adding table of user

---
+++ src/database/migrations/20190811014150-create-users.js
@@ -0,0 +1,28 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
+};
