08402e8677d6d1edba4454ac38ee5a4bc547cfae
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-08-10 23:51:06 -0300

    create: adding model of User

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ Message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Patrick Chagas',
+    email: 'patrick@teste.com',
+    password_hash: 'eyhr1918',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
